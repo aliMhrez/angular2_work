30a818a8ed6d3c44a9ca5c39e0bd6499b89c87a1
Ali Mhrez <alidman0@hotmail.com>
2017-08-06 11:47:36 +0300

    finish form validation

--- src/app/Item/item.component.ts
+++ src/app/Item/item.component.ts
@@ -30,6 +30,23 @@ export class ItemComponent implements OnInit {
     this.items.push(new Item(name, quantity, price));
   }
 
+  validate(name: string, quantity: number, price: number) {
+    const a = name.match('^([^0-9]*)$');
+
+    if (a === null || name.length < 3) {
+      document.getElementById('error1').hidden = false;
+    } else if (quantity < 0 || quantity > 20) {
+      document.getElementById('error2').hidden = false;
+    } else if (price < 0) {
+      document.getElementById('error3').hidden = false;
+    } else {
+      this.addNewItem(name, quantity, price);
+      document.getElementById('error1').hidden = true;
+      document.getElementById('error2').hidden = true;
+      document.getElementById('error3').hidden = true;
+    }
+  }
+
 //   handleKeyUp(ev) {
 //     if (ev.which === 13){
 //       this.addNewTask(ev.target.value);
